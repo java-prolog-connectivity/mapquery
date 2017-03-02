444b9d97e02064ae51c2465f0d4a9c1e756d88f4
Sergio Castro <Sergio.Castro@tomtom.com>
2017-03-02 14:20:02 +0100

    fix in coordinates converter

--- src/main/java/org/jpc/examples/osm/model/jpcconverters/CoordinatesJpcConverter.java
+++ src/main/java/org/jpc/examples/osm/model/jpcconverters/CoordinatesJpcConverter.java
@@ -2,13 +2,12 @@ package org.jpc.examples.osm.model.jpcconverters;
 
 import static java.util.Arrays.asList;
 
-import java.lang.reflect.Type;
-
+import org.jconverter.converter.TypeDomain;
 import org.jpc.Jpc;
-import org.jpc.mapping.converter.FromTermConverter;
-import org.jpc.mapping.converter.ToTermConverter;
 import org.jpc.examples.osm.model.Coordinates;
 import org.jpc.examples.osm.model.imp.CoordinatesJpc;
+import org.jpc.mapping.converter.FromTermConverter;
+import org.jpc.mapping.converter.ToTermConverter;
 import org.jpc.term.Compound;
 
 public class CoordinatesJpcConverter implements ToTermConverter<Coordinates, Compound>, FromTermConverter<Compound, Coordinates> {
@@ -16,14 +15,14 @@ public class CoordinatesJpcConverter implements ToTermConverter<Coordinates, Com
 	public static final String COORDINATE_FUNCTOR_NAME = "coordinates";
 	
 	@Override
-	public Coordinates fromTerm(Compound term, Type type, Jpc jpc) {
+	public Coordinates fromTerm(Compound term, TypeDomain typeDomain, Jpc jpc) {
 		double lon = jpc.fromTerm(term.arg(1));
 		double lat = jpc.fromTerm(term.arg(2));
 		return new CoordinatesJpc(lon, lat);
 	}
 
 	@Override
-	public Compound toTerm(Coordinates coordinates, Class<Compound> termClass, Jpc jpc) {
+	public Compound toTerm(Coordinates coordinates, TypeDomain typeDomain, Jpc jpc) {
 		return jpc.toCompound(COORDINATE_FUNCTOR_NAME, asList(coordinates.getLon(), coordinates.getLat()));
 	}
 
